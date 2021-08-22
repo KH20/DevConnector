e6b0557a2ed5c8445dacfcc4340ba0603d3e3583
HAMER Kurt <kurt.hamer@akka.eu>
2021-08-22 22:40:29 +0100

    Created JWT Token on user registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const { check, validationResult } = require("express-validator/check");
 
 const User = require("../../models/User");
@@ -57,21 +59,31 @@ router.post("/", [
         user.password = await bcrypt.hash(password, salt);
         await user.save();
 
-        //Return jsonwebtoken
-        res.send("User Registered");
+        //Set Payload
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
 
+        //Return jsonwebtoken
+        const token = jwt.sign(
+            payload,
+            config
+                .get("jwtSecret"),
+            { expiresIn: 360000 },
+            (err, token) => {
+                if (err) throw err;
+                return res.json({ token });
+            }
+        );
 
     } catch (err) {
         console.error(err.message);
-        res.status(500).send("Server Error");
+        return res
+            .status(500)
+            .send("Server Error");
     }
-
-
-
-
-
-
-    res.send("User Route");
 });
 
 module.exports = router;
