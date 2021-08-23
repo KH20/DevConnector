9c72ef3d6369ffd1dfa02f1ffb2259ac1ee8b2f9
KH20 <kurt.hamer.90@gmail.com>
2021-08-23 22:41:24 +0100

    Added github profile link

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const auth = require("../../middleware/auth");
 const Profile = require("../../models/Profile");
 const User = require("../../models/User");
+const request = require("request");
+const config = require("config");
 const { check, validationResult } = require("express-validator");
 
 //@router   GET api/profile/me
@@ -330,5 +332,31 @@ router.delete("/education/:edu_id", auth, async (req,res) => {
     }
 });
 
+//@router   GET api/profile/github/:username
+//@desc     Get User repos from Github
+//@access   Public
+router.get("/github/:username", (req,res) => {
+    try {
+        const options = {
+            uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get('githubClientId')}&client_secret=${config.get("githubSecret")}`, 
+            method: "GET",
+            headers: {"user-agent":"node.js"}
+        };
+
+        request(options, (error, response, body) => {
+            if(error) console.error(error);
+
+            if(response.statusCode !== 200){
+                return res.status(404).json({msg:"No github profile found"});
+            }
+
+            res.json(JSON.parse(body));
+        })
+
+    } catch (err) {
+        console.error(err.message);
+        return res.status(500).send("Server Error");
+    }
+});
 
 module.exports = router;
