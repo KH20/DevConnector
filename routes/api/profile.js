7a6a97214b7c687bb1d001911969b8a48d05d681
KH20 <kurt.hamer.90@gmail.com>
2021-08-23 21:40:14 +0100

    Added route for deleting profile experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -226,4 +226,26 @@ async (req,res) => {
 
 });
 
+//@router   DELETE api/profile/experience/:exp_id
+//@desc     Delete experience from profile
+//@access   Private
+
+router.delete("/experience/:exp_id", auth, async (req,res) => {
+    try {
+        const profile = await Profile.findOne({user: req.user.id});
+
+        //Get remove index
+        const removeIndex = profile.experience.map(item => item.id).indexOf(req.params.exp_id);
+        profile.experience.splice(removeIndex, 1);
+
+        await profile.save();
+
+        return res.json(profile);
+
+    } catch (error) {
+        console.error(error.message);
+        return res.status(500).send("Server Error");
+    }
+});
+
 module.exports = router;
