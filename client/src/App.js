89404f2f90684282a04d638ff05d59675720e365
KH20 <kurt.hamer.90@gmail.com>
2021-11-16 13:43:16 +0000

    Added Dashboard route

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ import { Landing } from "./components/layout/Landing";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import Alert from "./components/layout/Alert";
+import Dashboard from "./components/dashboard/Dashboard";
+import PrivateRoute from "./components/routing/PrivateRoute";
 
 //Redux
 import { Provider } from "react-redux";
@@ -41,6 +43,11 @@ const App = () => {
                                 path="/login"
                                 component={Login}
                             ></Route>
+                            <PrivateRoute
+                                exact
+                                path="/dashboard"
+                                component={Dashboard}
+                            ></PrivateRoute>
                         </Switch>
                     </section>
                 </Fragment>
